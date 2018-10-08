1022cc5fe34502828a6e4da47f951bcb4cf81abd
Scott Morse <scottmorsedev@gmail.com>
2018-10-08 15:19:07 -0500

    Final commit before first beta release.

--- setup.py
+++ setup.py
@@ -7,15 +7,21 @@ def readme():
 setup(name="pymusician",
       version="1.0.0-beta",
       description="A python package for music composition and analysis.",
+      long_description="""
+      Read the README here:
+      https://github.com/ScottMorse/PyMusician
+      This is a python package for representing musical structures. Its features have less to do with audio file production/management, and more with analysis and composition. However, the concepts represented here could be combined with other music/audio related code to make sophisticated musical projects.
+      """,
       url="https://github.com/ScottMorse/PyMusician",
       author="Scott Morse",
       author_email="scottmorsedev@gmail.com",
-      lisence="GPL",
+      lisence="Apache",
       packages=["pymusician"],
       include_package_data=True,
       install_requires=[
           'numpy',
       ],
+      python_requires='>3.6',
       test_suite='nose.collector',
       tests_require=['nose'],
       zip_safe=False)
